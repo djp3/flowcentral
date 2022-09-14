b741b668e344e857922baacfa09d40e1fea8b9be
tenglish19 <105843019+tenglish19@users.noreply.github.com>
2022-09-13 17:38:43 -0700

    Trevor's story
    You entered the cave

--- main.py
+++ main.py
@@ -42,5 +42,29 @@ def charles():
 
     elif choice == 2:
         valentina()
+        
+    else:
+        print("Bad answer!")
 
+def trevor()
+    print("""
+            You enter the cave. You valiantly ignite your torch, but you notice it will soon fade.
+            You gaze into the narrowing distant abyss. Behind a series of mossy stalagtites, there
+            appears to be a hobbit and a goblin-like character having a picnic.
+            
+            Do you:
+            1. Sneak up and kill them both, taking their food for yourself
+            2. Join them for a wholesome meal
+            
+            Type a number!
+            """)
+    choice = int(input("Type a number! "))
+
+    if choice == 1:
+        todd()
+    elif choice == 2:
+        ethan()
+    else:
+        print("Bad answer!")
+        
 main()
